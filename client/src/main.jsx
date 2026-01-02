4c570b54c3192ef560eeb3bb49c9abdbf5f9eb42
Rhane Corporation <rhanecorp@gmail.com>
2026-01-02 22:45:53 +0530

    add AuthContext and AuthProvider setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    <AuthContextProvider>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </AuthContextProvider>
   </BrowserRouter>
 );
