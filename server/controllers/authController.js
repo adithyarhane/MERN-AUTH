4ec10625636e5da477f38684346776482419e59c
Rhane Corporation <rhanecorp@gmail.com>
2026-01-16 03:04:24 +0530

    change  httpOnly false in production

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,7 +35,7 @@ export const register = async (req, res) => {
     const token = generateToken(user._id);
 
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: process.env.NODE_ENV === "production" ? false : true,
       secure: process.env.NODE_ENV === "production" ? true : false,
       sameSite: process.env.NODE_ENV === "production" ? None : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -71,7 +71,7 @@ export const login = async (req, res) => {
     const token = generateToken(user._id);
 
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: process.env.NODE_ENV === "production" ? false : true,
       secure: process.env.NODE_ENV === "production" ? true : false,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
