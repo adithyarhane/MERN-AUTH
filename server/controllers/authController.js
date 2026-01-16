9b6d012abba9b08102916c6f0549c34735e6618c
Rhane Corporation <rhanecorp@gmail.com>
2026-01-16 11:47:59 +0530

    fix cookie error

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production" ? true : false,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: process.env.NODE_ENV === "production" ? None : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       domain: ".onrender.com",
     });
